fac09c0828e7f15072cafaaca99d091b4e3b8d2f
Wilo92 <wilmerrestrepoo@hotmail.com>
2026-01-07 20:32:16 -0500

    07/01/2026

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        
+        "https://langgraphfront-production.up.railway.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
